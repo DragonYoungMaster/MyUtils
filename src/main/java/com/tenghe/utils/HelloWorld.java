5a1dac6e40b604a2a6b5cba8dce209f259684791
teng.he <teng.he@qunar.com>
2017-10-22 20:17:04 +0800

    modify

--- src/main/java/com/tenghe/utils/HelloWorld.java
+++ src/main/java/com/tenghe/utils/HelloWorld.java
@@ -5,6 +5,7 @@ package com.tenghe.utils;
  */
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("hello world!");
     }
 }
